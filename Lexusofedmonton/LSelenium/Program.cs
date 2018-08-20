e779b08774bf97ac33ad4acdb0e26e4e68c6c85f
vanhakobyan1996 <vanhakobyan1996@gmail.com>
2018-08-20 23:15:52 +0400

    add 2 variant

---
+++ Lexusofedmonton/LSelenium/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace LSelenium
+{
+    class Program
+    {
+        private static string link =
+            @"https://www.lexusofedmonton.ca/used-vehicles/#action=im_ajax_call&perform=get_results&page=1&type%5B%5D=Used&type%5B%5D=Certified+Used";
+        static void Main(string[] args)
+        {
+            var urls = new List<string>();
+            FirefoxProfile firefoxOption = new FirefoxProfile();
+            //firefoxOption.SetPreference("permissions.default.stylesheet", 2);
+            //firefoxOption.SetPreference("permissions.default.image", 2);
+            //firefoxOption.SetPreference("dom.ipc.plugins.enabled.libflashplayer.so", false);
+            FirefoxOptions fire = new FirefoxOptions();
+            fire.Profile = firefoxOption;
+            FirefoxDriver driver = new FirefoxDriver(fire);
+            driver.Navigate().GoToUrl(link);
+            for (int i = 1; i < 9; i++)
+            {
+                var pages = driver.FindElements(By.ClassName("pages"));
+                HtmlDocument document = new HtmlDocument();
+                document.LoadHtml(driver.PageSource);
+                var cars = document.DocumentNode.SelectNodes(".//div[@class='vehicle-title clearfix']");
+                foreach (var car in cars)
+                {
+                    var carDetail = car.SelectSingleNode(".//a");
+                    var url = carDetail.GetAttributeValue("href", "");
+                    if (!urls.Contains(url)) urls.Add(url);
+                }
+                if (i == 8) break;
+                pages.ElementAt(i).Click();
+                Thread.Sleep(1000);
+            }
+            
+            //File.WriteAllLines(@"E:\urls.txt", urls);
+
+        }
+    }
+}
---
+++ Lexusofedmonton/Lexusofedmonton/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace Lexusofedmonton
+{
+    class Program
+    {
+        private static string link =
+            @"https://www.lexusofedmonton.ca/used-vehicles/#action=im_ajax_call&perform=get_results&page={page}&type%5B%5D=Used&type%5B%5D=Certified+Used";
+        static void Main(string[] args)
+        {
+            var urls=new List<string>();
+            
+            for (int i = 1; i < 9; i++)
+            {
+                WebClient client = new WebClient();
+                client.Headers.Add(HttpRequestHeader.UserAgent, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36");
+                HtmlDocument document = new HtmlDocument();
+                var globalUrl = link.Replace("{page}", i.ToString());
+                var data = client.DownloadString(globalUrl);
+                document.LoadHtml(data);
+                var cars = document.DocumentNode.SelectNodes(".//div[@class='vehicle-title clearfix']");
+                foreach (var car in cars)
+                {
+                    var carDetail = car.SelectSingleNode(".//a");
+                    var url = carDetail.GetAttributeValue("href", "");
+                    if (!urls.Contains(url))urls.Add(url);
+                }
+            }
+
+            //File.WriteAllLines(@"E:\urls.txt",urls);
+        }
+    }
+}
