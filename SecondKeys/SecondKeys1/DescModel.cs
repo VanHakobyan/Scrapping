7e0ed526ea5a737ee3e2a642e0f196db5c9c1f8d
vanhakobyan <vanhakobyan1996@gmail.com>
2018-09-05 12:14:05 +0400

    add TVSCRAP

---
+++ SecondKeys/SecondKeys1/DescModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondKeys1
+{
+    public class DescModel
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity),Key]
+        public int Id { get; set; }
+        public string Model { get; set; }
+        public List<KeyValueDesc> KeyValueDesc { get; set; } = new List<KeyValueDesc>();
+    }
+
+    public class KeyValueDesc
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity),Key]
+        public int Id { get; set; }
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
---
+++ SecondKeys/SecondKeys1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace SecondKeys1
+{
+    class Program
+    {
+        private const string tvUrl = @"http://vesta.am/tv";
+        private const string dishwasherUrl = @"http://vesta.am/dishwasher/dishwashers";
+        private const string stoveUrl = @"https://vega.am/hy/oven#";
+        private const string refUrl = @"https://vega.am/hy/krupnaja-bytovaja-texnika/ref";
+
+        static void Main(string[] args)
+        {
+
+            WebClient client = new WebClient();
+            HtmlDocument documentTv = new HtmlDocument();
+            var stringTv = client.DownloadString(tvUrl);
+            documentTv.LoadHtml(stringTv);
+            var allContent = documentTv.DocumentNode.SelectNodes(".//div[@class='product-layout product-grid col-lg-4 col-md-4 col-sm-6 col-xs-12']").Select(x => x.SelectSingleNode(".//div[@class='image']/a/@href"));
+            var listOfTvLinks = new List<string>();
+            Regex regexTv = new Regex("<a href=\"(.*?)\"");
+            foreach (var tv in allContent)
+            {
+                listOfTvLinks.Add(regexTv.Match(tv.OuterHtml).Groups[1].Value);
+            }
+            List<DescModel> descModels = new List<DescModel>();
+            Model1 model1 = new Model1();
+            model1.Desc.AddRange(descModels);
+            foreach (var ofTvLink in listOfTvLinks)
+            {
+                try
+                {
+                    HtmlDocument document = new HtmlDocument();
+                    using (WebClient client1=new WebClient())
+                    {
+                        client1.Headers.Add(HttpRequestHeader.Cookie, "PHPSESSID=7f6fc6bc63d50c92c8f93e6860d2cf02; currency=AMD; _ga=GA1.2.1278618675.1536123905; _gid=GA1.2.542430229.1536123905; _a_d3t6sf=duYQ4AIPd9esVXtsghCLtcJy; language=am; _gat=1");
+                        document.LoadHtml(client1.DownloadString(ofTvLink)); 
+                    }
+                    var prodName = document.DocumentNode.SelectSingleNode(".//h1[@class='prodTitle']").InnerText;
+                    var prodDescript = document.DocumentNode.SelectSingleNode(".//div[@id='tab-description']").FirstChild.SelectNodes(".//tr");
+                    var descModel = new DescModel { Model = prodName };
+                    foreach (var desc in prodDescript)
+                    {
+                        try
+                        {
+                            var key = desc.SelectNodes(".//td")[0].InnerText;
+                            var value = desc.SelectNodes(".//td")[1].InnerText;
+                            descModel.KeyValueDesc.Add(new KeyValueDesc {Key = key,Value = value});
+                        }
+                        catch { }
+                    }
+                    descModels.Add(descModel);
+                    Thread.Sleep(1500);
+                }
+                catch
+                {
+                    Thread.Sleep(5000);
+                }
+            }
+            
+            model1.Desc.AddRange(descModels);
+            model1.SaveChanges();
+            Console.WriteLine();
+
+        }
+    }
+}
