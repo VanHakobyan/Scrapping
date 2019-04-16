49613b1ac5cb76ea45350c5dba0f1d8888ed6d6a
vanhakobyan <vanhakobyan1996@gmail.com>
2019-04-16 18:48:32 +0400

    add AW

---
+++ AutoWini/AutoWini/Service1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AutoWini
+{
+    public partial class Service1 : ServiceBase
+    {
+        private Task _task;
+
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Parser parser = new Parser();
+            _task = Task.Run(() => parser.GetAndAnalyzeData());
+        }
+
+        protected override void OnStop()
+        {
+            _task.Dispose();
+        }
+
+        public void TestStartupAndStop(string [] args)
+        {
+            Parser parser = new Parser();
+            parser.GetAndAnalyzeData();
+        }
+    }
+}
