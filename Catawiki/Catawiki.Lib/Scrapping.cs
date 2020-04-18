f899d090274461feaf58b25ba633209f460b2586
vanhakobyan1996 <vanhakobyan1996@gmail.com>
2020-04-18 21:45:54 +0400

    UI changes

--- Catawiki/Catawiki.Lib/Scrapping.cs
+++ Catawiki/Catawiki.Lib/Scrapping.cs
@@ -3,8 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HtmlAgilityPack;
 using OpenQA.Selenium.Chrome;
+using Scrapping.AllPossibilities;
+using Scrapping.AllPossibilities.Enums;
+using Scrapping.AllPossibilities.Http;
 using Scrapping.AllPossibilities.Selenium;
+using HtmlAttribute = Scrapping.AllPossibilities.Enums.HtmlAttribute;
 
 namespace Catawiki.Lib
 {
@@ -23,10 +28,16 @@ namespace Catawiki.Lib
             });
         }
 
-        public async Task<List<DataModel>> Start()
+        public async Task<List<DataModel>> Start(string link)
         {
             await Task.Delay(0);
-            _driver.Navigate().GoToUrl(Link);
+            _driver.Navigate().GoToUrl(string.IsNullOrEmpty(link) ? Link : link);
+
+            HtmlDocument document = new HtmlDocument();
+            document.LoadHtml(_driver.PageSource);
+
+            var navNode = HtmlDocumentHelper.GetNodeByParams(document.DocumentNode, "nav", "class", "be-pagination u-margin-t-large");
+            navNode.GetAttributeValue("name", null);
             //logic
             return new List<DataModel>();
         }
