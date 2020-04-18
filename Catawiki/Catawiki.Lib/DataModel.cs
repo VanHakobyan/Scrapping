a57a430f088cbefaf91c5aaa3a83dcd0b9687da3
vanhakobyan1996 <vanhakobyan1996@gmail.com>
2020-04-18 17:48:55 +0400

    add model

---
+++ Catawiki/Catawiki.Lib/DataModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catawiki.Lib
+{
+    public class DataModel
+    {
+        public decimal CurrentBidAmount { get; set; }
+        public string CurrentBid { get; set; }
+        public DateTime BiddingEndTime { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+}
