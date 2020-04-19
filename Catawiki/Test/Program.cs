129bbf55b9d3a4447df6acd3ed0ce11780a663c1
vanhakobyan1996 <vanhakobyan1996@gmail.com>
2020-04-19 14:56:12 +0400

    minor fix

--- Catawiki/Test/Program.cs
+++ Catawiki/Test/Program.cs
@@ -72,7 +72,7 @@ namespace Test
                             }
                             catch
                             {
-
+                                //ignore
                             }
                         }
 
