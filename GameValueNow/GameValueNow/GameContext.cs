bbdc938d36228a6b1b68b03994a007f3b59f55ae
vanhakobyan1996 <vanhakobyan1996@gmail.com>
2020-04-24 00:15:38 +0400

    minor fix

--- GameValueNow/GameValueNow/GameContext.cs
+++ GameValueNow/GameValueNow/GameContext.cs
@@ -9,12 +9,12 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace GameValueNow
-{ 
+{
     public class SQLiteConfiguration : DbConfiguration
     {
         public SQLiteConfiguration()
         {
-            SetProviderFactory("System.Data.SQLite", SQLiteFactory.Instance);
+            //SetProviderFactory("System.Data.SQLite", SQLiteFactory.Instance);
             SetProviderFactory("System.Data.SQLite.EF6", SQLiteProviderFactory.Instance);
             SetProviderServices("System.Data.SQLite", (DbProviderServices)SQLiteProviderFactory.Instance.GetService(typeof(DbProviderServices)));
         }
