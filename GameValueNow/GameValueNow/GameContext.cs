4c35bf63a09d967f06d5050407346da41ea6da56
manestep <manestep@gmail.com>
2020-04-23 20:02:24 +0400

    checking why i can't pull

--- GameValueNow/GameValueNow/GameContext.cs
+++ GameValueNow/GameValueNow/GameContext.cs
@@ -9,7 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace GameValueNow
-{
+{ 
     public class SQLiteConfiguration : DbConfiguration
     {
         public SQLiteConfiguration()
